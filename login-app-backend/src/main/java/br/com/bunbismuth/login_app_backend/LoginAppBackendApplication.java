832bb7fedd139e37808fb8f926a725e0d1c06a48
Thiago Barros <bunbismuth@proton.me>
2024-06-03 20:57:35 -0300

    build: backend project

---
+++ login-app-backend/src/main/java/br/com/bunbismuth/login_app_backend/LoginAppBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.bunbismuth.login_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginAppBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LoginAppBackendApplication.class, args);
+  }
+
+}
