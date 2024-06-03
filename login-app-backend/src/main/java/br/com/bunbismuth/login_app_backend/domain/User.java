03c6112915e6708dcb96f1e626b9b56c8faf5889
Thiago Barros <bunbismuth@proton.me>
2024-06-03 20:58:06 -0300

    feat: create User

---
+++ login-app-backend/src/main/java/br/com/bunbismuth/login_app_backend/domain/User.java
@@ -0,0 +1,30 @@
+package br.com.bunbismuth.login_app_backend.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private String id;
+
+  private String username;
+
+  private String email;
+
+  private String password;
+}
