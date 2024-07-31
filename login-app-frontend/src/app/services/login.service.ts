43da205800293b7f60f25b1a5e6e16f70a5b6621
Thiago Barros <bunbismuth@proton.me>
2024-07-31 19:50:42 -0300

    refactor: added backend route and other fixes

--- login-app-frontend/src/app/services/login.service.ts
+++ login-app-frontend/src/app/services/login.service.ts
@@ -1,20 +1,31 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { LoginResponse } from '../components/types/login-response.type';
+import { LoginResponse } from '../types/login-response.type';
 import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  apiUrl: string = "http://localhost:8080/auth"
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient) { }
 
-  login(email: string, password: string) {
-    return this.httpClient.post<LoginResponse>('/login', { email, password })
-      .pipe(tap((value) => {
-        localStorage.setItem('auth-token', value.token)
-        localStorage.setItem('username', value.name)
-      }))
+  login(email: string, password: string){
+    return this.httpClient.post<LoginResponse>(this.apiUrl + "/login", { email, password }).pipe(
+      tap((value) => {
+        sessionStorage.setItem("auth-token", value.token)
+        sessionStorage.setItem("username", value.name)
+      })
+    )
+  }
+
+  signup(name: string, email: string, password: string){
+    return this.httpClient.post<LoginResponse>(this.apiUrl + "/register", { name, email, password }).pipe(
+      tap((value) => {
+        sessionStorage.setItem("auth-token", value.token)
+        sessionStorage.setItem("username", value.name)
+      })
+    )
   }
 }
